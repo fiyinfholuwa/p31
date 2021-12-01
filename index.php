58131296c9cb52f7823636e32775ea57cbbae96f
fiyinfholuwa <fiyinfholuwa@gmail.com>
2021-12-01 20:03:18 +0100

    p31

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+	<title>P31 Hospitality</title>
+	
+
+</head>
+<link rel="stylesheet" type="text/css" href="style.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<body  style="background-image: url(bg.jpg); background-repeat: no-repeat; background-attachment: fixed; background-size: cover; height: 100vh;">
+<section>
+	<div class="text-center" id="btn">
+		<center><a href="download.php?file=p31" target="_blank" class="btn btn-primary btn-lg">Download P31 Hosptality Profile</a></center>
+	</div>
+</section>
+</body>
+</html>
+
